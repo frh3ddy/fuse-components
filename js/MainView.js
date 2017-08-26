972f97e85811b5e40ecece042ff7440e4a246961
Fredy Mendez <fr3ddy@me.com>
2017-08-26 10:55:52 -0400

    add Selectable

--- js/MainView.js
+++ js/MainView.js
@@ -7,35 +7,47 @@ export var customerName = Observable('')
 export var devicePassword = Observable('')
 export var phoneNumber = Observable('')
 export var price = Observable('')
+export var showing = Observable(true)
+
+export const selected = Observable('')
+export function showPicker () {
+  showing.value = true
+}
+
+selected.onValueChanged(module, value => {
+  console.log(value)
+  if (value === '') return
+  showing.value = false
+})
 
 handleFormatting(phoneNumber, '(xxx) xxx-xxxx')
 handleFormatting(price, '$xxx.xx')
 
 function handleFormatting (observable, TEMPLATE) {
-    const format = templateFormatter(TEMPLATE)
-    let lastFormattedValue
-  
-    observable.onValueChanged(module, value => {
-      if (value !== '') {
-        if (lastFormattedValue) {
+  const format = templateFormatter(TEMPLATE)
+  let lastFormattedValue
+
+  observable.onValueChanged(module, value => {
+    if (value !== '') {
+      if (lastFormattedValue) {
           // check if the value is actually different, to prevent a possible infinite loop
-          if (value !== lastFormattedValue) {
-            formatValue(value)
-          }
-        } else {
+        if (value !== lastFormattedValue) {
           formatValue(value)
         }
+      } else {
+        formatValue(value)
       }
-    })
-  
-    function formatValue (value) {
-      const numbers = getNumbersFromString(value)
-      lastFormattedValue = format(numbers).text
-      observable.value = format(numbers).text
     }
+  })
+
+  function formatValue (value) {
+    const numbers = getNumbersFromString(value)
+    lastFormattedValue = format(numbers).text
+    observable.value = format(numbers).text
   }
-  
-  function getNumbersFromString (string) {
-    let numbers = string.match(/\d/g)
-    return numbers ? numbers.join('') : ''
-  }
+}
+
+function getNumbersFromString (string) {
+  let numbers = string.match(/\d/g)
+  return numbers ? numbers.join('') : ''
+}
