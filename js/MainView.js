6d34dda9816accd9db685910d55f5fb14ceece30
Fredy Mendez <fr3ddy@me.com>
2017-09-27 11:03:37 -0400

    add Pagecontrol

--- js/MainView.js
+++ js/MainView.js
@@ -1,2 +1,12 @@
 import templateFormatter from 'fusejs_lib/inputFormatter'
 import Observable from 'FuseJS/Observable'
+
+export const activePage = Observable('Content')
+
+// export function handlePage () {
+//   if (activePage.value === 'Content') {
+//     activePage.value = 'test'
+//   } else {
+//     activePage.value = 'Content'
+//   }
+// }
